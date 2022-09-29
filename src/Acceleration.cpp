53e5b09d2ddfb9d2979e888db08cff6df685e90f
sbrown67 <70857567+sbrown67@users.noreply.github.com>
2022-09-29 13:04:23 -0500

    Update Acceleration.cpp

--- src/Acceleration.cpp
+++ src/Acceleration.cpp
@@ -1,6 +1,9 @@
 // ----------------------------------------------------------------------
 // <copyright file="Acceleration.cpp" company="6D">
-//     Copyright 2022. All right reserved
+//     	Copyright 2022. All right reserved
+//
+//	SPDX-FileCopyrightText: 2022 6th Dimension, Inc. <info@6thdpro.com>
+// 	SPDX-License-Identifier: GPL-3.0-or-later
 // </copyright>
 // ------------------------------------------------------------------------
 
