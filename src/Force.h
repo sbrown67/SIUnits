856373596cf2589a91a60055def25b14f7a598ce
sbrown67 <swbkkb@gmail.com>
2022-09-29 13:56:05 -0500

    latest with license

--- src/Force.h
+++ src/Force.h
@@ -1,6 +1,9 @@
 // ----------------------------------------------------------------------
 // <copyright file="Force.h" company="6D">
 //     Copyright 2022. All right reserved
+//
+//     SPDX-FileCopyrightText: 2022 6th Dimension, Inc. <info@6thdpro.com>
+//     SPDX-License-Identifier: GPL-3.0-or-later" + NewLine;
 // </copyright>
 // ------------------------------------------------------------------------
 
